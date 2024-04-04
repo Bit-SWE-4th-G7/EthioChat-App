bf63bd6b5db1b5fe35d247d5385b507f3a121ddd
EphriamHab <ephremhabtamu2015@gmail.com>
2024-04-04 11:23:26 +0300

    login screen

---
+++ lib/features/auth/screens/login_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  static const routeName = '/login-screen';
+  const LoginScreen({super.key});
+
+  State<LoginScreen> createState() => _LoginScreenState(); 
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Enter your phone number'),
+      ),
+    );
+  }
+}
