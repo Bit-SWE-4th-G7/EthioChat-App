d0613a4178881644bb256f7609e053f983142973
EphriamHab <ephremhabtamu2015@gmail.com>
2024-04-07 14:43:01 +0300

    user information

--- lib/features/auth/screens/user_information_screen.dart
+++ lib/features/auth/screens/user_information_screen.dart
@@ -1,27 +1,92 @@
+import 'dart:io';
 
-
+import 'package:ethiochat/common/utils/utils.dart';
+import 'package:ethiochat/features/auth/controller/auth_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class UserInformationScreen extends StatelessWidget {
+class UserInformationScreen extends ConsumerStatefulWidget {
   static const routeName = '/user-information';
+
+  ConsumerState<UserInformationScreen> createState() =>
+      _UserInformationScreenState();
+}
+
+class _UserInformationScreenState extends ConsumerState<UserInformationScreen> {
+  final TextEditingController nameController = TextEditingController();
+  File? image;
+
+  @override
+  void dispose() {
+    nameController.dispose();
+    super.dispose();
+  }
+
+  void selectImage() async {
+    image = await pickImageFromGallery(context);
+    setState(() {});
+  }
+  void storeUserData() async {
+      String name = nameController.text.trim();
+      if (name.isNotEmpty) {
+        ref.read(authControllerProvider).saveUserDataToFirebase(
+            context,
+             name,
+            image,
+          );
+      }
+    }
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
       body: SafeArea(
-        child: Center(
-          child: Column(
-            children: [
-              Stack(
-                children: [
-                  CircleAvatar(
-                    backgroundImage: NetworkImage(""),
+          child: Center(
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                image == null
+                    ? const CircleAvatar(
+                        backgroundImage:
+                            NetworkImage('https://picsum.photos/250?image=9'),
+                        radius: 64,
+                      )
+                    : CircleAvatar(
+                        backgroundImage: FileImage(image!),
+                        radius: 64,
+                      ),
+                Positioned(
+                    bottom: -10,
+                    left: 80,
+                    child: IconButton(
+                        onPressed: selectImage,
+                        icon: const Icon(
+                          Icons.add_a_photo,
+                        )))
+              ],
+            ),
+            Row(
+              children: [
+                Container(
+                  width: size.width * 0.85,
+                  padding: const EdgeInsets.all(20),
+                  child: TextField(
+                    controller: nameController,
+                    decoration: const InputDecoration(
+                      hintText: 'Enter Your name',
+                      labelText: 'Full Name',
+                      border: OutlineInputBorder(),
+                    ),
                   ),
-                  IconButton(onPressed: (){}, icon: Icon(Icons.add_a_photo))
-                ],
-              )
-            ],
-          ),
-          )),
+                ),
+                IconButton(onPressed: storeUserData, icon: Icon(Icons.done))
+              ],
+            ),
+          ],
+        ),
+      )),
     );
   }
 }
