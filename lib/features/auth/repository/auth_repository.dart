3f555bf0d53eccfbdc7cca4a928cda65a8c903aa
EphriamHab <ephremhabtamu2015@gmail.com>
2024-04-07 14:42:03 +0300

    auth repository

--- lib/features/auth/repository/auth_repository.dart
+++ lib/features/auth/repository/auth_repository.dart
@@ -1,9 +1,14 @@
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:ethiochat/common/repositories/common_firebase_storage_repository.dart';
 import 'package:ethiochat/common/utils/utils.dart';
 import 'package:ethiochat/features/auth/screens/otp_screen.dart';
 import 'package:ethiochat/features/auth/screens/user_information_screen.dart';
+import 'package:ethiochat/models/user_model.dart';
+import 'package:ethiochat/screens/mobile_layout_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final authRepositoryProvider = Provider((ref) => AuthRepository(
@@ -18,6 +23,15 @@ class AuthRepository {
     required this.auth,
     required this.firestore,
   });
+
+Future<UserModel?> getCurrentUserData() async {
+   var userData = await firestore.collection('users').doc(auth.currentUser!.uid).get();
+   UserModel? user;
+    if(userData.data() != null){
+      user = UserModel.fromMap(userData.data()!);
+    }
+    return user;
+  }
 
   void signInWithPhone(BuildContext context, String phoneNumber) async {
     try {
@@ -62,4 +76,44 @@ class AuthRepository {
       showSnackBar(context: context, content: e.message!);
     }
   }
+
+  void saveUserDataToFirebase({
+  required String name,
+  required File? profilePic,
+  required ProviderRef ref,
+  required BuildContext context,
+}) async {
+  try {
+    String uid = auth.currentUser!.uid;
+    String photoURL = 'https://png.pngitem.com/pimgs/s/649-6490124_katie-notopoulos-katienotopoulos-i-write-about-tech-round.png';
+
+    if (profilePic != null) {
+      photoURL = await ref
+          .read(commonFirebaseStorageRepositoryProvider)
+          .storeFileTOFirebase(
+            'profilePics/$uid', 
+            profilePic,
+          );
+    }
+    var user = UserModel(
+      name: name,
+      uid: uid,
+      profilePic: photoURL,
+      isOnline: true,
+      phoneNumber: auth.currentUser!.uid,
+      groupId: [],
+    );
+    await firestore.collection('users').doc(uid).set(user.toMap());
+    Navigator.pushAndRemoveUntil(
+      context,
+      MaterialPageRoute(
+        builder: (context) => const MobileLayoutScreen(),
+      ),
+      (route) => false,
+    );
+
+  } catch (e) {
+    showSnackBar(context: context, content: e.toString());
+    }
+ }
 }
