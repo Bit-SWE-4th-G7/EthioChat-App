af66b3798a4e5ffb5e82e4b3f7a1cd0631f38e0b
EphriamHab <ephremhabtamu2015@gmail.com>
2024-04-07 14:45:06 +0300

    select contact screen

---
+++ lib/features/select_contacts/screens/select_contacts_screen.dart
@@ -0,0 +1,61 @@
+import 'package:ethiochat/common/error.dart';
+import 'package:ethiochat/common/widgets/loader.dart';
+import 'package:ethiochat/features/select_contacts/controller/select_contact_contoller.dart';
+import 'package:ethiochat/widgets/contacts_list.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_contacts/contact.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class SelectContactsScreen extends ConsumerWidget {
+  static const String routeName = '/select-contact';
+
+  const SelectContactsScreen({super.key});
+
+  void selectContact(WidgetRef ref, Contact selectedContact, BuildContext context) async {
+    ref.read(selectContactControllerProvider).selectContact(selectedContact, context);
+
+  }
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Select Contact'),
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.search),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.more_vert),
+            ),
+          ],
+        ),
+        body: ref.watch(getContactsProvider).when(
+            data: (ContactsList) => ListView.builder(
+                itemCount: ContactsList.length,
+                itemBuilder: (context, index) {
+                  final contact = ContactsList[index];
+                  return InkWell(
+                    onTap: () => selectContact(ref, contact, context),
+                    child: Padding(
+                      padding: const EdgeInsets.only(bottom: 8.0),
+                      child: ListTile(
+                        title: Text(
+                          contact.displayName,
+                          style: const TextStyle(
+                            fontSize: 18,
+                            ),
+                          ),
+                        leading: contact.photo == null? null: CircleAvatar(
+                          backgroundImage: MemoryImage(contact.photo!),
+                          radius: 30,
+                        )
+                      ),
+                    ),
+                  );
+                }),
+            error: (err, trace) => ErrorScreen(error: err.toString()),
+            loading: () => const Loader()));
+  }
+}
