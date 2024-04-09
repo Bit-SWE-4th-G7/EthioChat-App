bc1d56c888df47d5991c2aaabcbead37382ff5df
EphriamHab <ephremhabtamu2015@gmail.com>
2024-04-09 20:02:30 +0300

    select contact

--- lib/features/select_contacts/screens/select_contacts_screen.dart
+++ lib/features/select_contacts/screens/select_contacts_screen.dart
@@ -1,7 +1,6 @@
 import 'package:ethiochat/common/error.dart';
 import 'package:ethiochat/common/widgets/loader.dart';
 import 'package:ethiochat/features/select_contacts/controller/select_contact_contoller.dart';
-import 'package:ethiochat/widgets/contacts_list.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_contacts/contact.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
